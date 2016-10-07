f6a64f1d85a6b9d44fd93a918cee13abf058d4b4
Brad Kilkenney <bkilkenney@gmail.com>
2016-10-07 14:55:47 -0400

    Initial Commit

---
+++ Inheritance11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            myCar.Year = 1989;
+            Console.WriteLine(myCar.Year);
+        }
+    }
+}
